d94ec7a588a12fbb6e04bcdca506a32e033df9cc
Wemerson Queiroz <wemersonqueiroz@yahoo.com>
2023-05-02 06:46:00 +0100

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "43356cba799b4ee8"; }
+/******/ 		__webpack_require__.h = function() { return "9fabbe1a1a246a1a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -11,14 +11,14 @@ export default function PageWrapper(props: PageWrapperProps) {
   const { darkMode } = useTheme()
 
   return (
-    <div className="z-[2] flex flex-col min-h-screen ">
-      <main
-        suppressHydrationWarning
-        className={`flex-1 flex h-full flex-col items-center justify-between px-4 relative pb-20 mt-20 ${
-          inter.className
-        } ${darkMode ? "dark" : ""}`}>
-        {props.children}
-      </main>
-    </div>
+    // <div className="z-[2] flex flex-col min-h-screen">
+    <main
+      suppressHydrationWarning
+      className={`flex-1 flex h-screen overflow-y-scroll flex-col items-center justify-between px-4 relative pt-20 pb-40 ${
+        inter.className
+      } ${darkMode ? "dark" : ""}`}>
+      {props.children}
+    </main>
+    // </div>
   )
 }
